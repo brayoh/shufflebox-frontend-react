6f330582232e55700fce0d6fbd7a47f0e6e55b7f
ianoti <ianodhiambootieno@gmail.com>
2017-09-12 15:13:26 +0300

    add cancel and confirm brownbag actions

--- src/actions/brownbagActions.js
+++ src/actions/brownbagActions.js
@@ -38,7 +38,7 @@ export function confirmBrownBag(brownBagObj){
   // this function is called when brownbags are done and status is changed to done
   // the brownBagObj should onky have "date", "status" and "id" fields.
   return dispatch => {
-    return fetch(`${fetchUrl}/api/brownbags/${brownBagObj.id}`, {
+    return fetch(`${fetchUrl}/api/brownbags/${brownBagObj.id}/`, {
       method: 'PUT',
       headers: {
         'Accept': 'application/json',
@@ -53,6 +53,24 @@ export function confirmBrownBag(brownBagObj){
   };
 }
 
+export function cancelBrownBag(brownBagObj){
+  // this function is called when brownbags are done and status is changed to done
+  // the brownBagObj should onky have "date", "status" and "id" fields.
+  return dispatch => {
+    return fetch(`${fetchUrl}/api/brownbags/${brownBagObj.id}/`, {
+      method: 'DELETE',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Authorization': process.env.TOKEN
+      },
+      credentials: 'include',
+      body: JSON.stringify(brownBagObj)
+    })
+    .then(dispatch({ type: actions.CONFIRM_BROWNBAG_CANCEL }))
+    .catch(dispatch({ type: actions.BROWNBAG_ACTION_ERROR }));
+  };
+}
 export function requestNextPresenters(presenter) {
   return {
     type: actions.BROWNBAG_NEXT_PRESENTER,
