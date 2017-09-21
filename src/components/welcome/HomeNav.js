b570866d596990fe6fb3c14504448752dfb9aa33
Tonida Baraza <tonida.baraza@andela.com>
2017-09-21 23:52:07 +0300

    add tab navigation to home page

--- src/components/welcome/HomeNav.js
+++ src/components/welcome/HomeNav.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router';
+import { Link } from 'react-router-dom';
 import { Navbar, Nav, NavItem, Tabs } from 'react-bootstrap';
 
 const styles = require('./HomeNav.scss');
