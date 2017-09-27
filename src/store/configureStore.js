e06c737b2086c5018a755a094f311d50fe3441b3
Tonida Baraza <tonida.baraza@andela.com>
2017-09-28 00:29:18 +0300

    add redux logger

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,6 +1,7 @@
 import { createStore, applyMiddleware } from 'redux';
 import { apiMiddleware } from 'redux-api-middleware';
 import thunk from 'redux-thunk';
+import logger from 'redux-logger';
 import reduxImmutableStateInvariant from 'redux-immutable-state-invariant';
 import rootReducer from '../reducers';
 
@@ -8,6 +9,6 @@ export default function configureStore(initialState) {
   return createStore(
     rootReducer,
     initialState,
-    applyMiddleware(apiMiddleware, thunk, reduxImmutableStateInvariant())
+    applyMiddleware(apiMiddleware, thunk, reduxImmutableStateInvariant(), logger)
   );
 }
