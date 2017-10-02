4d4c5518648c61008946d5d481d310c715484082
Tonida Baraza <tonida.baraza@andela.com>
2017-10-02 13:36:45 +0300

    add redux devTools

--- src/redux/store/configureStore.js
+++ src/redux/store/configureStore.js
@@ -3,12 +3,15 @@ import { apiMiddleware } from 'redux-api-middleware';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
 import reduxImmutableStateInvariant from 'redux-immutable-state-invariant';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import rootReducer from '../reducers';
 
 export default function configureStore(initialState) {
   return createStore(
     rootReducer,
     initialState,
-    applyMiddleware(apiMiddleware, thunk, reduxImmutableStateInvariant(), logger)
+    composeWithDevTools(
+      applyMiddleware(apiMiddleware, thunk, reduxImmutableStateInvariant(), logger) 
+    )
   );
 }
