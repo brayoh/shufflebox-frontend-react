ef4ad09c0537c11ba6f3d55869e618dce8702d9b
brayoh <bkuriakb@gmail.com>
2017-10-09 16:44:47 +0300

    [CHORE] Change directory structure for scss files

--- src/index.js
+++ src/index.js
@@ -4,11 +4,13 @@ import { render } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import Routes from './routes';
 // import './styles/styles.css';
-import './styles/main.scss';
 
 // Import Provider from redux and configure store
 import {Provider} from 'react-redux';
 import configureStore from './redux/store/configureStore';
+
+import './styles/main.scss';
+import './styles/styles.scss';
 
 // Set initialState as {} for now
 const store = configureStore({});
